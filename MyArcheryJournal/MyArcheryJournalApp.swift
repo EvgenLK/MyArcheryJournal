b2213503bada0a9187fee4e093fdd21ac61b573b
EvgenLK <120525967+EvgenLK@users.noreply.github.com>
2024-07-01 20:52:48 +1000

    Initial Commit

---
+++ MyArcheryJournal/MyArcheryJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyArcheryJournalApp.swift
+//  MyArcheryJournal
+//
+//  Created by Evgenii Kutasov on 01.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct MyArcheryJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
